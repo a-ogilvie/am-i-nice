0311adbd9137bc2262f404e74aaefa8749c80b56
a-ogilvie <alexander.ogilvie@icloud.com>
2018-05-15 14:56:05 +0900

    Add facebook API fetch

---
+++ public/javascripts/fb.js
@@ -0,0 +1,65 @@
+function statusChangeCallback(response) {
+  console.log("statusChangeCallback");
+  console.log(response);
+  if (response.status === "connected") {
+    fetchPosts(response.authResponse);
+  } else {
+    document.getElementById("status").innerHTML = "Please log into this app.";
+  }
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: "192011154757596",
+    cookie: true,
+    xfbml: true,
+    version: "v3.0"
+  });
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+};
+
+(function(d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
+
+let userPosts = [];
+
+function fetchPosts(userInfo) {
+  FB.api(
+    `/${userInfo.userID}/posts`,
+    "get",
+    { access_token: userInfo.accessToken },
+    pageResults
+  );
+}
+
+function pageResults(result) {
+  result.data.forEach((datum) => {
+    if (datum.message) {
+      userPosts.push(datum.message);
+    }
+  });
+
+  if (userPosts.length < 100 && result.paging && result.paging.next) {
+    FB.api(result.paging.next, "get", null, pageResults);
+  } else {
+    analyseSentiments(userPosts);
+  }
+}
