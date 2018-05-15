d029b9b5e97c6e7707265f9ae6eb5cddd65402c6
a-ogilvie <alexander.ogilvie@icloud.com>
2018-05-15 15:11:41 +0900

    Revert ES6 change for router

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
 
-import indexRouter from "./routes/index";
+const indexRouter = require("./routes/index");
 
 const app = express();
 
