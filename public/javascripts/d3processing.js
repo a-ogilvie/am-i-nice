9ff292f278827ff329b6eca9b36e1818cb319343
a-ogilvie <alexander.ogilvie@icloud.com>
2018-05-15 16:22:33 +0900

    Add legend

--- public/javascripts/d3processing.js
+++ public/javascripts/d3processing.js
@@ -3,6 +3,16 @@ function drawGraph(sentimentality) {
   const height = 360;
   const radius = Math.min(width, height) / 2;
   const donutWidth = 75;
+  const legendRectSize = 18;
+  const legendSpacing = 4;
+
+  const positiveColour = "#008000";
+  const neutralColour = "#C0C0C0";
+  const negativeColour = "#FF0000";
+
+  const colour = d3
+    .scaleOrdinal()
+    .range([positiveColour, neutralColour, negativeColour]);
 
   const svg = d3
     .select("#result")
@@ -28,8 +38,32 @@ function drawGraph(sentimentality) {
     .enter()
     .append("path")
     .attr("d", arc)
-    .attr("fill", (d) => {
-      console.log(d);
-      return d.data.colour;
+    .attr("fill", (d) => colour(d.data.label));
+
+  const legend = svg
+    .selectAll(".legend")
+    .data(colour.domain())
+    .enter()
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", (d, i) => {
+      const height = legendRectSize + legendSpacing;
+      const offset = height * colour.domain().length / 2;
+      const horizontal = -2 * legendRectSize;
+      const vertical = i * height - offset;
+      return `translate(${horizontal},${vertical})`;
     });
+
+  legend
+    .append("rect")
+    .attr("width", legendRectSize)
+    .attr("height", legendRectSize)
+    .style("fill", colour)
+    .style("stroke", colour);
+
+  legend
+    .append("text")
+    .attr("x", legendRectSize + legendSpacing)
+    .attr("y", legendRectSize - legendSpacing)
+    .text((d) => d);
 }
