c2d5d645e990eff0cdf6f752c9282d83c0a2e323
a-ogilvie <alexander.ogilvie@icloud.com>
2018-05-15 14:56:26 +0900

    Add POST routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var router = express.Router();
 
+const language = require("@google-cloud/language");
+const client = new language.LanguageServiceClient();
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Am I Nice?" });
+});
+
+router.post("/sentiment", (req, res) => {
+  const post = {
+    content: req.body.post,
+    type: "PLAIN_TEXT"
+  };
+
+  client.analyzeSentiment({ document: post }).then((results) => {
+    const sentiment = results[0].documentSentiment;
+    return res.send(sentiment);
+  });
 });
 
 module.exports = router;
