a7aef0b187c2464f29be4b4617aa88e5daf39a8c
a-ogilvie <alexander.ogilvie@icloud.com>
2018-05-15 16:22:10 +0900

    Remove ejs templating

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ const indexRouter = require("./routes/index");
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
