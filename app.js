af3da7be6b531631bfcf739ce84a94f9d0669879
a-ogilvie <alexander.ogilvie@icloud.com>
2018-05-15 17:44:55 +0900

    Change routing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
 
-const indexRouter = require("./routes/index");
+const sentimentRouter = require("./routes/sentiment");
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/sentiment", sentimentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
